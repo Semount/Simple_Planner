f46c6e35a5677a4f0329e3f30527e0ce11a0341e
Semount <semount228@yandex.ru>
2023-08-02 20:02:49 +0300

    Programm's skeleton is done

--- Simple_Planner/MainWindow.xaml.cs
+++ Simple_Planner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Simple_Planner.Models;
 
 namespace Simple_Planner
 {
@@ -20,14 +22,44 @@ namespace Simple_Planner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<PlannerModel> _PlannerData;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _PlannerData = new BindingList<PlannerModel>()
+            {
+                new PlannerModel {Text = "First"},
+                new PlannerModel {Text = "Second"}
+            };
+
+            TaskList.ItemsSource = _PlannerData;
+        }
+
+        bool collapse = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Collapse(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Love to you");
+            if (collapse == false)
+            {
+                this.Left_Bar.Width = new GridLength(35, GridUnitType.Pixel);
+                this.Collapse_button.Content = "->";
+                collapse = true;
+            }
+            else
+            {
+                this.Left_Bar.Width = new GridLength(180, GridUnitType.Pixel);
+                this.Collapse_button.Content = "Collapse";
+                collapse = false;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Simple_Planner/Models/PlannerModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Planner.Models
+{
+    internal class PlannerModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _IsDone;
+        private string _Text;
+
+        public bool IsDone 
+        { 
+            get { return _IsDone; }
+            set { _IsDone = value; }
+        }
+        public string Text
+        {
+            get { return _Text; }
+            set { _Text = value; }
+        }
+    }
+}
